6103355c524f934441aa72621bf54687cd85f53d
tonyadms1509 <tonyadams1509@gmail.com>
2026-02-11 16:16:21 +0200

    Set up React Router routes in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";          // your main app (Dashboard/Home)
+import EnvTest from "./EnvTest";  // your environment test component
 
-import { AuthProvider } from './hooks/useAuth';
-import { LanguageProvider } from './hooks/useLocalization';   // <-- requires useLocalization.tsx in hooks/
-import { CurrencyProvider } from './hooks/useCurrency';
-import { ToastProvider } from './hooks/useToast';
-import { DataProvider } from './hooks/useDataContext';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <LanguageProvider>
-        <CurrencyProvider>
-          <ToastProvider>
-            <DataProvider>
-              <App />
-            </DataProvider>
-          </ToastProvider>
-        </CurrencyProvider>
-      </LanguageProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/envtest" element={<EnvTest />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
